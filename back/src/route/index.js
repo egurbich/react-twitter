4e3799c8eb70fa542d1ec2a5b00548e1c1346d61
Yevhen Hurbych <eugene.gurbych@gmail.com>
2024-06-06 07:26:03 +0200

    React. Twitter.

--- back/src/route/index.js
+++ back/src/route/index.js
@@ -1,18 +1,12 @@
-// Підключаємо роутер до бек-енду
 const express = require('express')
 const router = express.Router()
 
-// Підключіть файли роутів
-// const test = require('./test')
-// Підключіть інші файли роутів, якщо є
+const post = require('./post')
 
-// Об'єднайте файли роутів за потреби
-// router.use('/', test)
-// Використовуйте інші файли роутів, якщо є
+router.use('/', post)
 
 router.get('/', (req, res) => {
   res.status(200).json('Hello World')
 })
 
-// Експортуємо глобальний роутер
 module.exports = router
